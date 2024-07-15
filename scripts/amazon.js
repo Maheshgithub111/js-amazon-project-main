536ae731a055866c1cf58deaa25bf14ced7529b8
Mahesh <maheshhsingh69@gmail.com>
2024-07-15 22:29:34 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -66,20 +66,23 @@ button.addEventListener("click", () => {
     let matchingItem;
 
     cart.forEach((item) => {
-    if (productId === item.productId) {
-    matchingItem = item;
-    }
+        if (productId === item.productId) {
+        matchingItem = item;
+        }
     });
 
-    if (matchingItem) {
-    matchingItem.quantity += 1;
-    } else {
-    cart.push({
-        productId: productId,
-        quantity: 1,
-    });
+        if (matchingItem) {
+        matchingItem.quantity += 1;
+        } else {
+        cart.push({
+            productId: productId,
+            quantity: 1,
+        });
     }
-
-    console.log(cart);
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+        cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 });
 });
